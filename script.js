faba8051f7586b89fa733017d7a30668893c5ac3
Hemlata-22 <hemlatarawat295@gmail.com>
2025-06-14 00:14:28 +0530

    all files added commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var tl =gsap.timeline();
+
+tl.from(" #box1", {
+    x: 2000,
+    duration: 2
+})
+tl.from(" #box2", {
+    x: 2000,
+    duration: 2  
+})
+tl.from(" #box3", {
+    x: 2000,
+    duration: 2
+})
+tl.from(" #box4", {
+    x: 2000,
+    duration: 2  
+})
+tl.from(" #box5", {
+    x: 2000,
+    duration: 2
+})
+tl.from(" #box6", {
+    x: 2000,
+    duration: 2  
+})
